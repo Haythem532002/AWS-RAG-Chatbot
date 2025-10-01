e6b6f0f3cc0507d10b349a3c49a2eaaaedffd6c3
Haythem532002 <khiarih06@gmail.com>
2025-10-01 23:06:03 +0100

    finish working on load data file

--- load_data.py
+++ load_data.py
@@ -1,8 +1,31 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-
+from langchain_chroma import Chroma
+from embedding_function import create_embedding_function
+import os
+import shutil
+import argparse
 DATA_PATH = "data"
+CHROMA_PATH = "chroma"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--reset",
+        action="store_true",
+        help="Reset the database"
+    )
+    args = parser.parse_args()
+
+    if args.reset:
+        print("Clearing the database")
+        clear_database()
+    
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
 
 def load_documents():
     document_loader = PyPDFDirectoryLoader(DATA_PATH)
@@ -17,6 +40,54 @@ def split_documents(documents: list[Document]):
     )
     return splitter.split_documents(documents)
 
-documents = load_documents()
-chunks = split_documents(documents)
-print(chunks[0])
+
+def add_to_chroma(chunks: list[Document]):
+    db=Chroma(
+        persist_directory=CHROMA_PATH,
+        collection_name="rag-chatbot",
+        embedding_function=create_embedding_function
+    )
+    chunks_with_ids = calculate_chunk_id(chunks)
+
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+
+    print(f"Number of existing documents in DB : {len(existing_ids)}")
+
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] is not in existing_ids:
+            new_chunks.append(chunk)
+    if(len(new_chunks)):
+        print(f"Adding new documents : {len(new_chunks)}")
+        new_chunks_ids = [chunk.metadate["id"] for chunk in new_chunks]
+        db.add_document(new_chunks, ids=new_chunks_ids)
+        db.persist()
+    else:
+        print("No new documents to add")
+    
+
+def calculate_chunk_id(chunks):
+    
+    last_page_id = None
+    current_chunk_index = 0
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index=0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        chunk.metadata["id"] = chunk_id
+    return chunks
+
+def clear_database():
+    if(os.path.exists(CHROMA_PATH)):
+        shutil.rmtree(CHROMA_PATH)
+    
